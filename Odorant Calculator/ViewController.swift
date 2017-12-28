399d881372b9981ba8812c5f31621974440d578d
Russell Brown <russell.brown67@googlemail.com>
2017-12-28 15:10:02 +0000

    Initial Commit

---
+++ Odorant Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Odorant Calculator
+//
+//  Created by Russell Brown on 28/12/2017.
+//  Copyright © 2017 TCD Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
